17b0b2f745c773ad1eb93d1837d60e0cd2d7e311
AliKORKMAZ53 <ahmetkorkmaz9853@gmail.com>
2021-10-28 17:06:00 +0300

    index.html fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+    http_basic_authenticate_with name:"ahmet",password:"mostar",
+    only: :destroy
+    
     def create
         @article= Article.find(params[:article_id])
         @comment= @article.comments.create(comment_params)
@@ -10,7 +13,7 @@ class CommentsController < ApplicationController
         @comment.destroy
         redirect_to article_path(@article)
     end
-    
+
 
     private 
     def comment_params
